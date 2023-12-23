d28f42a7f92a91788310644bd7222ae75b9d16e5
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2023-12-23 17:17:01 +0530

    Create array_methods.js

---
+++ JavaScript/array_methods.js
@@ -0,0 +1,20 @@
+let name=[1,2,3,4,"This is Names"]
+/*console.log(name)
+name[4]=5*/
+console.log(name)
+
+
+
+let b = name.toString()
+console.log(b)
+console.log(name.join("-"))
+
+
+let c = name.pop();
+console.log(c)
+
+let d = name.push("90")
+console.log(d)
+
+
+
