2ace0b3a07dfc19c32a0870380e326a67bd5df04
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2023-12-23 11:55:19 +0530

    Create string_methods.js

---
+++ JavaScript/string_methods.js
@@ -0,0 +1,9 @@
+let s = "Replit"
+
+console.log(s.length)
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+console.log(s.slice(2,5))
+console.log(s.replace("Re" , "HR"))
+console.log(s,"Best Software")
+console.log(s.trim())
