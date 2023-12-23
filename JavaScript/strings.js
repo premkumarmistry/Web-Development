c8972f0be35ab2bc1113e2abd71a0f436b78b424
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2023-12-23 11:32:33 +0530

    Create strings.js
    String Usage

---
+++ JavaScript/strings.js
@@ -0,0 +1,12 @@
+let s = "Replit"
+console.log(s.length)
+let c = 'This is Single Qoute String in Javascript'
+//so now we are printing it to console using console function
+console.log(c)
+//now we are printing its length using length function of JS
+console.log(c.length)
+//using backticks
+let a = "prem"
+let b ="mistry"
+let CC= `The Suraname of ${a} is ${b}`
+console.log(CC)
